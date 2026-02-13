3d56b6946a3712b7278dbf06b003a2e6d09501da
Hein <u6632106@au.edu>
2026-02-13 14:51:40 +0700

    edit app.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { Items } from "./Items";
 import { ItemDetail } from "./ItemDetail";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Items />} />
-        <Route path="/edit/:id" element={<ItemDetail />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Items />} />
+      <Route path="/edit/:id" element={<ItemDetail />} />
+    </Routes>
   );
 }
 
